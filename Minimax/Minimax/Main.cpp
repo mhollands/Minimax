f2bb835275d292c5d9f58828cb5e8f8941aea107
mhollands <mh739@cam.ac.uk>
2014-11-16 01:16:19 +0000

    Added Othello board
    not fully tested yet but seems ok

--- Minimax/Minimax/Main.cpp
+++ Minimax/Minimax/Main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-#include "NnC_Board.cpp"
+//#include "NnC_Board.cpp"
+#include "Othello_Board.cpp"
 
 int main()
 {
+	
+	Board_Base* b = new Othello_Board();
+	b->print();
+
+	for (int i = 0; i < 10; i++)
+	{
+		MinimaxEngine engine = MinimaxEngine(b);
+		Board_Base* newMove = engine.getSuggestedMove(0, true);
+
+		delete b;
+		b = newMove;
+		b->print();
+		std::cout << endl;
+
+		int newPos;
+		cin >> newPos;
+
+		b->setPiece(newPos + 1000);
+		b->print();
+		std::cout << endl;
+	}
+
+	std::cout << "Game Over" << endl;
+	delete b;
+
+	int a;
+	cin >> a;
+	
+	/*
 	int spaces[9] = { 1, 1, -1, -1, -1, 1, 1, 0, 0 };
 	Board_Base* board = new NnCBoard();
 	board->print();
@@ -23,7 +53,7 @@ int main()
 		board->print();
 		std::cout << endl;
 
-		if (board->countPossibleMoves(false) == 0)
+		if (board->calculateHeuristic() != 0 || board->countPossibleMoves(false) == 0)
 		{
 			break;
 		}
@@ -41,4 +71,5 @@ int main()
 
 	int blah;
 	cin >> blah;
+	*/
 };
---
+++ Minimax/Minimax/Othello_Board.cpp
@@ -0,0 +1,239 @@
+#include "Base_Header.h"
+
+class Othello_Board : public Board_Base
+{
+private:
+	int spaces[8][8];
+
+	bool applyMove(int space, bool player)
+	{
+		int spaceX = space % 8;
+		int spaceY = space / 8;
+		if (spaces[spaceX][spaceY] != 0)
+		{
+			return false;
+		}
+
+		spaces[spaceX][spaceY] = player ? 1 : -1;
+
+		bool validMove = false;
+
+		for (int i = -1; i <= 1; i++)
+		{
+			for (int j = -1; j <= 1; j++)
+			{
+				if (i == 0 && j == 0)
+				{
+					continue;
+				}			
+
+				int v = 0;
+				int checkSpaceX = spaceX;
+				int checkSpaceY = spaceY;
+				bool started = false;
+				for (v = 1; (checkSpaceX = spaceX + v*i) < 8 & (checkSpaceY = spaceY + v*j) < 8
+						&& checkSpaceX >= 0 && checkSpaceY >= 0; v++)
+				{
+					bool quitVector = false;
+					switch(spaces[checkSpaceX][checkSpaceY])
+					{
+					case 0:
+						quitVector = true;
+						break;
+
+					case 1:
+						if (player)
+						{
+							if (started)
+							{
+								validMove = true;
+							}
+							quitVector = true;
+							break;
+						}
+
+						started = true;
+						break;
+
+					case -1:
+						if (!player)
+						{
+							if (started)
+							{
+								validMove = true;
+							}
+							quitVector = true;
+							break;
+						}
+
+						started = true;
+						break;
+					}
+
+					if (quitVector)
+					{
+						break;
+					}
+				}
+
+				if (validMove)
+				{
+					bool ended = false;
+					while(checkSpaceX != spaceX || checkSpaceY != spaceY)
+					{
+						spaces[checkSpaceX][checkSpaceY] = (player ? 1 : -1);
+						checkSpaceX -= i;
+						checkSpaceY -= j;
+					}
+				}
+
+			}
+		}
+
+		return validMove;
+	}
+
+	Othello_Board* privateClone()
+	{
+		int newSpaces[8][8];
+
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				newSpaces[i][j] = spaces[i][j];
+			}
+		}
+
+		return new Othello_Board(newSpaces);
+	}
+
+public:
+
+	Othello_Board()
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if ((i == 3 || i == 4))
+				{
+					if (i == j)
+					{
+						spaces[i][j] = 1;
+						continue;
+					}
+
+					if (i == 7 - j)
+					{
+						spaces[i][j] = -1;
+						continue;
+					}
+				}
+
+				spaces[i][j] = 0;
+			}
+		}
+	}
+
+	Othello_Board(int newSpaces[8][8])
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				spaces[i][j] = newSpaces[i][j];
+			}
+		}
+	}
+
+	int countPossibleMoves(bool player)
+	{
+		return 0;
+	}
+
+	vector<Board_Base*> listPossibleMoves(bool player)
+	{
+		vector<Board_Base*> newMoves;
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (spaces[i][j] == 0)
+				{
+					Othello_Board* newMove = privateClone();
+
+					if (newMove->applyMove(i + j * 8, player))
+					{
+						newMoves.push_back(newMove);
+						continue;
+					}
+
+					delete newMove;
+				}
+			}
+		}
+
+		return newMoves;
+	}
+
+	bool moveIsLegal(Board_Base* newMove)
+	{
+		return false;
+	}
+
+	void print()
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				switch (spaces[i][j])
+				{
+				case 0:
+					cout << ".";
+					break;
+				case 1:
+					cout << "o";
+					break;
+				case -1:
+					cout << "x";
+					break;
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	int calculateHeuristic()
+	{
+		int score = 0;
+		for (int j = 0; j < 8; j++)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				score += spaces[i][j];
+			}
+		}
+
+		return score;
+	}
+
+	Board_Base* clone()
+	{
+		return privateClone();
+	}
+
+	void setPiece(long data)
+	{
+		if (data > 1000)
+		{
+			data -= 1000;
+			applyMove(data, false);
+			return;
+		}
+
+		applyMove(data, true);
+		return;
+	}
+};
